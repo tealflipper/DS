ce0f4d70f5cb27ab4215b1e9742c6a7340ba0eee
tealflipper <arturofv009@gmail.com>
2020-02-24 21:30:34 -0600

    works

--- Lab_4/3_node.c
+++ Lab_4/3_node.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h> 
 #include <mpi.h>
-#define N 4
+#define N 24
 #define STAR 4 // nodes in star topology network
-#define MASTER 4 /*El nodo maestro será el ultimo proceso, al igual que en los subgrupos*/
+#define MASTER 24 /*El nodo maestro será el ultimo proceso, al igual que en los subgrupos*/
 #define TRUE 1
 #define FALSE 0
 
@@ -22,20 +22,21 @@ int getSum(int* arr, int n){
 }
 int * getNeighbors(int iRank){
   int localRank=iRank%STAR;
-  int* neighbors=(int*)calloc(N,sizeof(int));
-  if(localRank==0){
-    //int prev=(iRank==0)? 20:iRank-4;
-    //int next=(iRank==lastNode)? 0: iRank+4;
-    //neighbors[prev]=1;
-    //neighbors[next]=1;
-    for (int i=1;i<=3;i++){
-      neighbors[iRank+i]=1;
+  int n=(localRank==0)?5:1;
+  int* neighbors=(int*)calloc(n,sizeof(int));
+  for(int i=0;i<n;i++){
+    if(localRank==0){//6 indices
+      int prev=(iRank==0)? 20:iRank-4;
+      int next=(iRank==20)? 0: iRank+4;
+      neighbors[0]=prev;
+      neighbors[1]=next;
+      for (int i=2;i<6;i++){
+        neighbors[i]=iRank+i;
+      }
+
     }
+    else neighbors[i]=iRank-localRank;
   }
-  else neighbors[iRank-localRank]=1;
-  neighbors[iRank]=1;
-  //Para poder agregar nodo maestro
-  neighbors[MASTER]=1;
   return neighbors;
 }
 /*
@@ -64,8 +65,8 @@ int main (int iArg, char *spcArgv[])
   // Get the rank and size in the original communicator
 
   MPI_Init (&iArg, &spcArgv);
-int world_rank, world_size;
-
+int world_rank, world_size,iEnvio=0,visited=-1,father=-1;
+int elements;
 MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
@@ -73,45 +74,33 @@ MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 MPI_Group world_group;
 MPI_Comm_group(MPI_COMM_WORLD, &world_group);
 
-int n = 4;
-int outside=3,inside=5;
-int** inGroup=(int**)malloc(sizeof(int*)*n);
-int** ranks=(int**)malloc(sizeof(int*)*n);
-int tmp;
-ranks[0]=getNeighbors(0);
-for (int i = 1; i<n;i++){
-  ranks[i]=getNeighbors(i);
-}
-for(int i=0;i<n;i++){
-  int elements=getSum(ranks[i],n+1);
-  inGroup[i]=(int*)calloc(elements,sizeof(int));
-  int k=0; //index of elements in array
-  for(int j=0;j<n+1;j++){
-    if(ranks[i][j]==1){
-      inGroup[i][k++]=j;
-    }
-  }
-}
-/*
-for(int i=0;i<n;i++){ 
-  int tmp=(i%STAR==0)?5:3;
-  printf("[%d]: ",world_rank);
-  for(int j=0;j<tmp;j++) printf("%d ", inGroup[i][j]);
-  printf("\n");
-}
-*/
-// Construct a group containing all of the prime ranks in world_group
+int n = N;
+int outside=1,inside=5;
+int*inGroup;
+int* ranks=getNeighbors(world_rank);
+
+printf("[%d]: ",world_rank);
+for(int i=0;i<elements;i++) printf("%d ",getNeighbors(world_rank));
+MPI_Barrier (MPI_COMM_WORLD); //Esperamos a los nodos
+  //printf("ahora el nodo %d creara su grupo\n",world_rank); //Esperamos a los nodos
+// construye N grupos
 MPI_Group my_group[n];
 for(int i=0;i<n;i++) {
-  int tmp=(i%STAR==0)?5:3;
-  MPI_Group_incl(world_group, tmp, inGroup[i], &my_group[i]);
+  int tmp=(i%STAR==0)?7:3;
+  MPI_Group_incl(world_group, tmp, inGroup, &my_group[i]);
 }
 
-// Create a new communicator based on the group
+// crea N comunicadores
 MPI_Comm prime_comm[n];
 for(int i=0;i<n;i++) {
-  int tmp=(i%STAR==0)?5:3;
+  int tmp=(i%STAR==0)?7:3;
   MPI_Comm_create_group(MPI_COMM_WORLD, my_group[i], i, &prime_comm[i]);
+  if ((prime_comm[i]==MPI_COMM_NULL)&&(iIsInGroup(inGroup[i],elements, world_rank)))
+  { //Si esta en el grupo y no se pudo crear
+    printf ("Error en comunicadores Grupo %d, nodo %d.\n",i, world_rank);
+    MPI_Finalize ();
+    return (0);
+  }
 }
 
 int prime_rank[n], prime_size[n];
@@ -119,9 +108,7 @@ for(int i=0;i<n;i++) {
   prime_rank[i]=-1;
   prime_size[i]=-1;
 }
-// If this rank isn't in the new communicator, it will be
-// MPI_COMM_NULL. Using MPI_COMM_NULL for MPI_Comm_rank or
-// MPI_Comm_size is erroneous
+//assigna rank con comunicador y talla si el comunicador no es nulo
 for(int i=0;i<n;i++) 
 {
   if (MPI_COMM_NULL != prime_comm[i]) {
@@ -129,10 +116,35 @@ for(int i=0;i<n;i++)
     MPI_Comm_size(prime_comm[i], &prime_size[i]);
   }
 }
-for(int i=0;i<n;i++) printf("el nodo %d en el grupo %d %d\n",world_rank,i,prime_rank[i]);
+//grupos creados
+//broadcast here
+if(world_rank==0){
+  visited=1;
+  //envio id a vecinos
+  MPI_Bcast(&world_rank,1,MPI_INT,0,prime_comm[0]);
+  //no hay padre
+  father=-1;
+  //enviar padre a maestro
+  MPI_Send(&father,1,MPI_INT,world_size-1,2,MPI_COMM_WORLD);
+
+  //espero padres
+  int Fathers[world_size-1];
+  MPI_Recv(&Fathers[0],world_size-1,MPI_INT,world_size-1,2,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+  for (int i = 0; i < world_size - 1; i++)
+    printf("Proceso %d, mi padre es %d\n", i, Fathers[i]);
+    
+}else if(world_rank<world_rank-1){
+  //recive from father
+  
+}
+
+
+
+//for(int i=0;i<n;i++) printf("el nodo %d en el grupo %d %d\n",world_rank,i,prime_rank[i]);
+for(int i=0;i<n;i++){
+   MPI_Group_free(&my_group[i]);
+}
 MPI_Group_free(&world_group);
-for(int i=0;i<n;i++) MPI_Group_free(&my_group[i]);
-//MPI_Comm_free(&prime_comm);
 
  MPI_Finalize ();
  return (0);
